d17ae565690d41f951ab11e1c8f903a87c84ca51
Anna <anna.vm@mail.ru>
2022-01-25 12:23:20 +0300

    Добавьте файлы проекта.

---
+++ калькулятор/Models/Calcul.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace калькулятор.Models
+{
+    class Calcul
+    {
+        static public double SqrtCalcul(double a) => Math.Sqrt(a);
+        static public double PowerCalcul(double a, double b) => Math.Pow(a, b);
+
+    }
+}
